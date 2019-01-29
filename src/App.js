87f0ee6d9962e4ea42f341aece3df82f505d2b83
ricky <ricky@rickys-MacBook-Air.local>
2019-01-29 15:15:53 -0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
-import Nav from "./components/Nav";
+// import React, { Component } from "react";
+// import Card from "./components/Card";
+// import Wrapper from "./components/Wrapper";
+// import Title from "./components/Title";
+// import friends from "./friends.json";
+// import Nav from "./components/Nav";
 
 
-class App extends Component {
+// class App extends Component {
   
-  state = {
-    friends
-  };
+//   state = {
+//     friends
+//   };
 
-  removeFriend = id => {
+//   removeFriend = id => {
     
-    const friends = this.state.friends.filter(friend => friend.id !== id);
+//     const friends = this.state.friends.filter(friend => friend.id !== id);
 
-    this.setState({ friends });
-  };
+//     this.setState({ friends });
+//   };
 
-  render() {
-    return (
+//   render() {
+//     return (
       
-      <Wrapper>
-        <Nav/>
-        <Title>Click Game</Title>
-        {this.state.friends.map(friend => (
-          <Card
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
+//       <Wrapper>
+//         <Nav/>
+//         <Title>Click Game</Title>
+//         {this.state.friends.map(friend => (
+//           <Card
+//             removeFriend={this.removeFriend}
+//             id={friend.id}
+//             key={friend.id}
+//             name={friend.name}
+//             image={friend.image}
+//             occupation={friend.occupation}
+//             location={friend.location}
+//           />
+//         ))}
+//       </Wrapper>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from "react";
+import Game from "./components/Game";
+
+function App() {
+  return <Game />;
 }
 
 export default App;
