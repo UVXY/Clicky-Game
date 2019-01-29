fd09b2c7f6baac53ca4d2eb267eb0a7b7d5b775a
ricky <ricky@rickys-MacBook-Air.local>
2019-01-29 15:14:55 -0800

    Updates

---
+++ precache-manifest.bd79dced3e52f6aa04636b94fb269082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b5c8c4a057414feb129",
+    "url": "/GameofClicks/static/js/runtime~main.7b5c8c4a.js"
+  },
+  {
+    "revision": "b946621e09221b546a81",
+    "url": "/GameofClicks/static/js/main.b946621e.chunk.js"
+  },
+  {
+    "revision": "441b227c7c9e041cd9b4",
+    "url": "/GameofClicks/static/js/1.441b227c.chunk.js"
+  },
+  {
+    "revision": "b946621e09221b546a81",
+    "url": "/GameofClicks/static/css/main.0e579fd1.chunk.css"
+  },
+  {
+    "revision": "bedc1de1acc2d4422e7f1af86dd963ea",
+    "url": "/GameofClicks/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a3ab8fd62b2e3cdf8d68a9f1750411f.js"
+  "/GameofClicks/precache-manifest.bd79dced3e52f6aa04636b94fb269082.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/GameofClicks/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
